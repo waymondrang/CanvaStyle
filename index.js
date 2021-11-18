5f1f9ff72b8f7049b16ec50bd35a846a93797076
raymond wang <waymondrang@users.noreply.github.com>
2021-11-18 07:54:11 -0800

    more cleanup

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ function randomint(min, max) {
 
 dm4cl("extension activated");
 
-function attachimage(image) {
-    //inject images.js
-    var img = document.createElement('img');
-    img.setAttribute("src", chrome.extension.getURL(image));
-    img.classList.add("dm4c-image", "hidden");
-    document.body.insertBefore(img, document.body.lastChild);
-}
-
 //declare head element
 const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
 
@@ -33,45 +25,3 @@ css.rel = "stylesheet";
 document.body.insertBefore(css, document.body.lastChild);
 
 dm4cl("injected css")
-
-/* //inject images.js
-const imagesjs = document.createElement('script');
-imagesjs.setAttribute("src", chrome.extension.getURL('images.js'));
-imagesjs.id = "dm4c-images-module";
-imagesjs.async = false;
-document.body.insertBefore(imagesjs, document.body.lastChild);
-
-attachimage("purin_pancakes.png") */
-
-/* console.log("running groupsinit")
-
-function ababa() {
-    var parent = document.querySelector(".planner-today");
-    parent.style.position = "relative";
-    var img = document.createElement('img');
-    img.setAttribute("src", chrome.extension.getURL("pompurrin.png"));
-    img.classList.add("dm4c-image");
-    img.style.top = `${-100}px`;
-    img.style.left = `${140}px`;
-    parent.insertBefore(img, parent.lastChild);
-}
-
-var groupsinit = document.querySelector(".Grouping-styles__root");
-if (!groupsinit) {
-    var groupsobserver = new MutationObserver(function (mutations, me) {
-        var groups = document.querySelector(".planner-today");
-        if (groups) {
-            dm4cl("ooga booga");
-            ababa();
-            me.disconnect(); //STOP OBSERVING
-        }
-    });
-    //START OBSERVING
-    groupsobserver.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
-} else {
-    dm4cl("ooga booga");
-    ababa();
-} */
